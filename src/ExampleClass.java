8b776ff607b6034ac6946a20310e994c3e8fb701
Yuvraj Patel <yuvipatel003@gmail.com>
2020-10-30 15:53:08 -0400

    Added initial project.

---
+++ src/ExampleClass.java
@@ -0,0 +1,13 @@
+public class ExampleClass {
+
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+        demo.printFunction();
+    }
+}
+
+class Demo {
+    public void printFunction(){
+        System.out.println("Fun : printFunction, Class : Demo");
+    }
+}
